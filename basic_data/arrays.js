e0816d3ace29d4115be1b5847c9dfc205d2e1c87
Traore <my123gate@gmail.com>
2019-05-03 00:55:02 -0400

    each individual subscription triggers a new call of the observable

--- basic_data/arrays.js
+++ basic_data/arrays.js
@@ -1,19 +1,14 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const rxjs_1 = require("rxjs");
-const members_1 = __importDefault(require("../members"));
-const member$ = rxjs_1.from(members_1.default);
 //#region Creating an observer
-const memberObserver = {
-    next: (member) => console.log('The value is: ' + member.firstName),
-    error: () => console.log('An error has happened'),
-    complete: () => console.log('No more streaming is left')
-};
-//Subscribing to the observable with a predefined observer object.
-member$.subscribe(memberObserver);
-//Injecting an observer in an observable
-member$.subscribe((member) => console.log(`${member.firstName} is : ${member.age}`), () => console.log('An error has happened'), () => console.log('No more streaming is left'));
+//Each subscription to an observable triggers it's execution
+//Then leading to a new data streaming 
+const timeObservable$ = new rxjs_1.Observable(subscriber => {
+    const currentTime = new Date().toLocaleTimeString();
+    subscriber.next(currentTime);
+});
+timeObservable$.subscribe((value) => console.log('subscriber 1 time is: ' + value));
+setTimeout(() => timeObservable$.subscribe((value) => console.log('subscriber 2 time is: ' + value)), 1000);
+setTimeout(() => timeObservable$.subscribe((value) => console.log('subscriber 3 time is: ' + value)), 2000);
 //#endregion
--- basic_data/arrays.ts
+++ basic_data/arrays.ts
@@ -1,22 +1,25 @@
-import { of,from } from 'rxjs';
+import { Observable, Subscriber} from 'rxjs';
 
 import members from '../members';
-const member$ = from(members);
 
 //#region Creating an observer
+//Each subscription to an observable triggers it's execution
+//Then leading to a new data streaming 
 
-const memberObserver = {
-    next: (member: any) => console.log('The value is: '+member.firstName),
-    error: () => console.log('An error has happened'),
-    complete: ()=> console.log('No more streaming is left')
-}
+const timeObservable$ = new Observable(subscriber => {
+    const currentTime = new Date().toLocaleTimeString();
+    subscriber.next(currentTime);
+});
 
-//Subscribing to the observable with a predefined observer object.
-    member$.subscribe(memberObserver);
-//Injecting an observer in an observable
-member$.subscribe(
-    (member: any) => console.log(`${member.firstName} is : ${member.age}`),
-    () => console.log('An error has happened'),
-    ()=> console.log('No more streaming is left')
+timeObservable$.subscribe(
+    (value: any) => console.log('subscriber 1 time is: '+ value),
 )
+
+setTimeout(() => timeObservable$.subscribe(
+    (value: any) => console.log('subscriber 2 time is: '+ value),
+),1000);
+
+setTimeout(() => timeObservable$.subscribe(
+    (value: any) => console.log('subscriber 3 time is: '+ value),
+),2000);
 //#endregion
