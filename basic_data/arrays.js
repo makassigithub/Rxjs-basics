693d7e750ff38ee28d738eae0a2a96f6f504f2e6
Traore <my123gate@gmail.com>
2019-05-05 23:45:49 -0400

    build a custom operator

--- basic_data/arrays.js
+++ basic_data/arrays.js
@@ -3,13 +3,34 @@ exports.__esModule = true;
 var operators_1 = require("rxjs/operators");
 var ajax_1 = require("rxjs/ajax");
 var xmlhttprequest_1 = require("xmlhttprequest");
+var rxjs_1 = require("rxjs");
 var url = 'http://0.0.0.0:8080/members';
 function createXHR() {
     return new xmlhttprequest_1.XMLHttpRequest();
 }
 //Creating my own operator by wrapping a built-in one:
-function memberFilterOperator() {
-    return operators_1.filter(function (member) { return member.age >= 35; }),
-        operators_1.tap(function (member) { return console.log('tap-FirstName: ' + member.firstName); });
+function memberFilterOperator(age, log) {
+    // returns  a function that will be called with the obeservable.
+    return function (source$) {
+        //This function must return an observable for the next operator 
+        //Or to be subscribe to.
+        return new rxjs_1.Observable(function (subscriber) {
+            //must subscribe to the incoming obsevable.
+            source$.subscribe(
+            //value
+            function (member) {
+                if (member.age >= age) {
+                    if (log) {
+                        console.log(member.firstName + ': passed');
+                    }
+                    subscriber.next(member);
+                }
+            }, 
+            //error
+            function (err) { return subscriber.error(err); }, 
+            //completed
+            function () { return subscriber.complete(); });
+        });
+    };
 }
-ajax_1.ajax({ createXHR: createXHR, url: url }).pipe(operators_1.mergeMap(function (members) { return members.response; }), memberFilterOperator()).subscribe(function (member) { return console.log("subs-member: " + JSON.stringify(member)); });
+ajax_1.ajax({ createXHR: createXHR, url: url }).pipe(operators_1.flatMap(function (members) { return members.response; }), memberFilterOperator(35, true)).subscribe(function (member) { return console.log("subs-member: " + JSON.stringify(member)); });
--- basic_data/arrays.ts
+++ basic_data/arrays.ts
@@ -1,6 +1,8 @@
-import { mergeMap, filter, tap } from 'rxjs/operators';
+import { flatMap, filter, tap } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 import { XMLHttpRequest } from 'xmlhttprequest';
+import { Observable } from 'rxjs';
+import { Member } from './model';
 
 const url = 'http://0.0.0.0:8080/members';
 
@@ -9,13 +11,37 @@ function createXHR() {
 }
 
 //Creating my own operator by wrapping a built-in one:
-function memberFilterOperator(){
-  return filter(member => member.age >= 35 ),
-  tap( member => console.log('tap-FirstName: '+member.firstName))
+function memberFilterOperator(age: number, log: boolean){
+  // returns  a function that will be called with the obeservable.
+  return (source$: Observable<Member>) => {
+    //This function must return an observable for the next operator 
+    //Or to be subscribe to.
+    return new Observable(
+      subscriber => {
+        //must subscribe to the incoming obsevable.
+        source$.subscribe(
+           //value
+          member => {
+            if(member.age >= age){
+              if(log){
+                console.log(member.firstName+ ': passed');
+              }
+              subscriber.next(member);
+            }
+          },
+          //error
+          err => subscriber.error(err),
+          //completed
+          () => subscriber.complete()
+        )
+      }
+    )
+  }
 }
+
 ajax({createXHR,url: url }).pipe(
-    mergeMap(members => members.response),
-    memberFilterOperator(),
+    flatMap(members => members.response),
+    memberFilterOperator(35,true),
 ).subscribe(
-    member => console.log("subs-member: "+JSON.stringify(member))
+    (member: Member) => console.log("subs-member: "+JSON.stringify(member))
 )
