9f8341073d0773a67d3345b026ccaed8e4dc2b90
Traore <my123gate@gmail.com>
2019-05-09 00:11:51 -0400

    custom hot Observable multicasting

--- basic_data/subjects.js
+++ basic_data/subjects.js
@@ -2,14 +2,16 @@
 exports.__esModule = true;
 var rxjs_1 = require("rxjs");
 var operators_1 = require("rxjs/operators");
-// Implemeting Cold Observable unicasting.
-// Each subcscription triggers an excution of the observable
-// Because the producer is inside the observable
-var timeInter$ = rxjs_1.interval(10).pipe(operators_1.take(4));
-timeInter$.subscribe(function (value) { return console.log('Observer 1: ' + value); });
+// Implemeting Hot Observable multicasting.
+// By introducting a subject in between the observables and the
+// Observers, any time a value is produced, it is passes to them all.
+var timeInter$ = rxjs_1.interval(1000).pipe(operators_1.take(4));
+var timeSubject$ = new rxjs_1.Subject();
+timeInter$.subscribe(timeSubject$);
+timeSubject$.subscribe(function (value) { return console.log('Observer 1: ' + value); });
 setTimeout(function () {
-    timeInter$.subscribe(function (value) { return console.log('Observer 2: ' + value); });
+    timeSubject$.subscribe(function (value) { return console.log('Observer 2: ' + value); });
 }, 1000);
 setTimeout(function () {
-    timeInter$.subscribe(function (value) { return console.log('Observer 3: ' + value); });
+    timeSubject$.subscribe(function (value) { return console.log('Observer 3: ' + value); });
 }, 2000);
--- basic_data/subjects.ts
+++ basic_data/subjects.ts
@@ -1,26 +1,30 @@
 import { Observable, Subject, interval } from 'rxjs';
 import { take } from 'rxjs/operators';
 
-// Implemeting Cold Observable unicasting.
-// Each subcscription triggers an excution of the observable
-// Because the producer is inside the observable
+// Implemeting Hot Observable multicasting.
+// By introducting a subject in between the observables and the
+// Observers, any time a value is produced, it is passes to them all.
 
-const timeInter$ = interval(10).pipe(
+
+const timeInter$ = interval(1000).pipe(
     take(4)
 )
 
-timeInter$.subscribe(
+const timeSubject$ = new Subject();
+timeInter$.subscribe(timeSubject$);
+
+timeSubject$.subscribe(
     value => console.log('Observer 1: '+ value),
 )
 
 setTimeout(() => {
-    timeInter$.subscribe(
+    timeSubject$.subscribe(
         value => console.log('Observer 2: '+ value),
     )
 }, 1000);
 
 setTimeout(() => {
-    timeInter$.subscribe(
+    timeSubject$.subscribe(
         value => console.log('Observer 3: '+ value),
     )
 }, 2000);
