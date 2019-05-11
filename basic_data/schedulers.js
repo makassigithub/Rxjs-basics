e5dc468313d042683073e5a2c295a75b46e2e9db
Traore <my123gate@gmail.com>
2019-05-11 00:42:13 -0400

    butin scheduler operators

--- basic_data/schedulers.js
+++ basic_data/schedulers.js
@@ -1,10 +1,22 @@
 "use strict";
 exports.__esModule = true;
 var rxjs_1 = require("rxjs");
+var operators_1 = require("rxjs/operators");
 // Scheduler allow us to have fine grained control over how the observables are executed.
-console.log('++++++++srcipt starts executing++++++');
-var queue$ = rxjs_1.of('queueScheduler', rxjs_1.queueScheduler); //runs synchronously
-var asap$ = rxjs_1.of('asapScheduler', rxjs_1.asapScheduler); // runs asychronously with higher priority than normal queued tasks.
-var async$ = rxjs_1.of('asyncScheduler', rxjs_1.asyncScheduler); // Run asynchronously.
-rxjs_1.merge(async$, asap$, queue$).subscribe(function (value) { return console.log(value); });
-console.log('+++++++++++Script ended executing+++++++');
+// console.log('++++++++srcipt starts executing++++++');
+// const queue$ = of('queueScheduler', queueScheduler); //runs synchronously
+// const asap$ = of('asapScheduler', asapScheduler); // runs asychronously with higher priority than normal queued tasks.
+// const async$ = of('asyncScheduler', asyncScheduler); // Run asynchronously.
+// merge(async$,asap$,queue$).subscribe(value => console.log(value));
+// console.log('+++++++++++Script ended executing+++++++');
+/* OUTPUT:
+    ++++++++srcipt starts executing++++++
+    queueScheduler
+    +++++++++++Script ended executing+++++++
+    asapScheduler
+    asyncScheduler
+*/
+// Scheduler operator are use to differe potential heavy operation to avoid blocking
+console.log('script start executing....................');
+rxjs_1.from([1, 2, 3, 4]).pipe(operators_1.tap(function (value) { return console.log("VALUE: " + value); }), operators_1.observeOn(rxjs_1.asyncScheduler), operators_1.tap(function (value) { return console.log("DOUBLED VALUE: " + value * 2); })).subscribe();
+console.log('script stops executing.....................');
--- basic_data/schedulers.ts
+++ basic_data/schedulers.ts
@@ -1,15 +1,16 @@
-import { asyncScheduler, asapScheduler, queueScheduler, of, merge } from 'rxjs';
+import { asyncScheduler, asapScheduler, queueScheduler, of, merge, from } from 'rxjs';
+import { tap, observeOn } from 'rxjs/operators';
 
 // Scheduler allow us to have fine grained control over how the observables are executed.
 
-console.log('++++++++srcipt starts executing++++++');
+// console.log('++++++++srcipt starts executing++++++');
 
-const queue$ = of('queueScheduler', queueScheduler); //runs synchronously
-const asap$ = of('asapScheduler', asapScheduler); // runs asychronously with higher priority than normal queued tasks.
-const async$ = of('asyncScheduler', asyncScheduler); // Run asynchronously.
+// const queue$ = of('queueScheduler', queueScheduler); //runs synchronously
+// const asap$ = of('asapScheduler', asapScheduler); // runs asychronously with higher priority than normal queued tasks.
+// const async$ = of('asyncScheduler', asyncScheduler); // Run asynchronously.
 
-merge(async$,asap$,queue$).subscribe(value => console.log(value));
-console.log('+++++++++++Script ended executing+++++++');
+// merge(async$,asap$,queue$).subscribe(value => console.log(value));
+// console.log('+++++++++++Script ended executing+++++++');
 
 /* OUTPUT:
     ++++++++srcipt starts executing++++++
@@ -17,4 +18,27 @@ console.log('+++++++++++Script ended executing+++++++');
     +++++++++++Script ended executing+++++++
     asapScheduler
     asyncScheduler
+*/
+
+// Scheduler operator are use to differe potential heavy operation to avoid blocking
+
+console.log('script start executing....................');
+from([1,2,3,4]).pipe(
+    tap(value => console.log(`VALUE: ${value}`)),
+    observeOn(asyncScheduler),
+    tap(value => console.log(`DOUBLED VALUE: ${value * 2}`))
+).subscribe();
+console.log('script stops executing.....................');
+
+/* OUTPUT:
+script start executing....................
+VALUE: 1
+VALUE: 2
+VALUE: 3
+VALUE: 4
+script stops executing.....................
+DOUBLED VALUE: 2
+DOUBLED VALUE: 4
+DOUBLED VALUE: 6
+DOUBLED VALUE: 8
 */
